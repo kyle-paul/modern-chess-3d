953aec53040f0eb1f0717519d76ceec9d9acd9dd
Kyle Paul <kylepaul1501@gmail.com>
2024-11-18 14:35:49 +0700

    basic setup to render chessboard and imgui for debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,209 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
+#include <imgui.h>
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <string>
+#include <vector>
+#include <iostream>
 
-int main(void)
+float vertices[12 * 3] = {
+    -4.0, -4.0, 0.5,
+    -4.0,  4.0, 0.5,
+     4.0,  4.0, 0.5,
+     4.0, -4.0, 0.5,
+
+    -4.5, -4.5, 0.5,
+    -4.5,  4.5, 0.5,
+     4.5,  4.5, 0.5,
+     4.5, -4.5, 0.5,
+      
+    -5.0, -5.0, 0.0,
+    -5.0,  5.0, 0.0,
+     5.0,  5.0, 0.0,
+     5.0, -5.0, 0.0,
+};
+
+unsigned int indices[12] = {
+    0, 1, 2, 3,
+    4, 5, 6, 7,
+    8, 9, 10, 11
+};
+
+glm::vec3 scale_vector = {0.1f, 0.1f, 0.1f};
+glm::vec3 rotation_vector = {0.2f, 0.3f, 0.7f};
+
+void initialization()
+{
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
+}
+
+void keyFunction(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void renderChessBoard()
+{
+    std::string vertexSource = R"(
+        #version 330 core
+        layout(location = 0) in vec3 position;
+        layout(location = 1) in vec3 color;
+
+        out vec3 fragColor;
+
+        uniform mat4 transform;
+
+        void main()
+        {
+            gl_Position = transform * vec4(position, 1.0);
+            fragColor = color;
+        }
+    )";
+    std::string fragmentSource = R"(
+        #version 330 core
+        in vec3 fragColor;
+        out vec4 color;
+
+        void main()
+        {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    )";
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar *source = (const GLchar *)vertexSource.c_str();
+    glShaderSource(vertexShader, 1, &source, 0);
+    glCompileShader(vertexShader);
+
+    GLint isCompiled = 0;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
+    if(isCompiled == GL_FALSE)
+    {
+        GLint maxLength = 0;
+        glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<GLchar> infoLog(maxLength);
+        glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+        glDeleteShader(vertexShader);
+        return;
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    source = (const GLchar *)fragmentSource.c_str();
+    glShaderSource(fragmentShader, 1, &source, 0);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
+    if (isCompiled == GL_FALSE)
+    {
+        GLint maxLength = 0;
+        glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<GLchar> infoLog(maxLength);
+        glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+        glDeleteShader(fragmentShader);
+        glDeleteShader(vertexShader);
+        return;
+    }
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    
+    GLint isLinked = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, (int *)&isLinked);
+    if (isLinked == GL_FALSE)
+    {
+        GLint maxLength = 0;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+        std::vector<GLchar> infoLog(maxLength);
+        glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+        glDeleteProgram(program);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);	
+        return;
+    }
+
+    glDetachShader(program, vertexShader);
+    glDetachShader(program, fragmentShader);
+
+    // ==================
+
+    glUseProgram(program);
+    GLuint VAO, VBO, IBO;
+
+    // Set up vertex array
+    glCreateVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Set up vertex buffer
+    glCreateBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Position attribute (location 0)
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glm::mat4 transform = glm::toMat4(glm::quat(rotation_vector)) * glm::scale(glm::mat4(1.0f), scale_vector);
+    GLint location = glGetUniformLocation(program, "transform");
+    glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(transform));
+    
+    glBindVertexArray(VAO);
+    glDrawElements(GL_QUADS, 12, GL_UNSIGNED_INT, nullptr);
+    glUseProgram(0);
+}
+
+void displayFunction() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.2f, 0.2f, 0.2f, 0.8f);
+    renderChessBoard();
+}
+
+void initImGui(GLFWwindow *window)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+
+    if (!ImGui_ImplOpenGL3_Init("#version 410")) {
+        std::cerr << "ImGui OpenGL3 Init failed!\n";
+        return;
+    }
+
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; 
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+}
+
+void renderImGui()
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("Debug Console");
+    ImGui::DragFloat3("Object Rotation", glm::value_ptr(rotation_vector), 0.1, 0, 1);
+    ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+int main()
 {
     GLFWwindow* window;
 
@@ -21,13 +223,30 @@ int main(void)
         return -1;
     }
 
+    initialization();
+    glfwSetKeyCallback(window, keyFunction);
+
+    initImGui(window);
+
     while (!glfwWindowShouldClose(window))
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
+    {        
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+
+        displayFunction();
+        renderImGui();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
