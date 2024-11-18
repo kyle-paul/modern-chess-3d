fa8141272018d3b7545c65b82be2a8108e535681
Kyle Paul <kylepaul1501@gmail.com>
2024-11-18 17:16:06 +0700

    play around with the camera perspective frustum and camera view matrices

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,102 @@
 #include <glm/gtc/type_ptr.hpp>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <imgui.h>
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <string>
 #include <vector>
 #include <iostream>
+#include <cmath>
 
-float vertices[12 * 3] = {
-    -4.0, -4.0, 0.5,
-    -4.0,  4.0, 0.5,
-     4.0,  4.0, 0.5,
-     4.0, -4.0, 0.5,
+float vertices[16 * 6] = {
+    -4.0, -4.0, 0.5, 0.55, 0.24, 0.09,
+    -4.0,  4.0, 0.5, 0.55, 0.24, 0.09,
+     4.0,  4.0, 0.5, 0.55, 0.24, 0.09,
+     4.0, -4.0, 0.5, 0.55, 0.24, 0.09,
 
-    -4.5, -4.5, 0.5,
-    -4.5,  4.5, 0.5,
-     4.5,  4.5, 0.5,
-     4.5, -4.5, 0.5,
+    -4.5, -4.5, 0.5, 0.55, 0.24, 0.09,
+    -4.5,  4.5, 0.5, 0.55, 0.24, 0.09,
+     4.5,  4.5, 0.5, 0.55, 0.24, 0.09,
+     4.5, -4.5, 0.5, 0.55, 0.24, 0.09,
       
-    -5.0, -5.0, 0.0,
-    -5.0,  5.0, 0.0,
-     5.0,  5.0, 0.0,
-     5.0, -5.0, 0.0,
+    -5.0, -5.0, 0.0, 0.55, 0.24, 0.09,
+    -5.0,  5.0, 0.0, 0.55, 0.24, 0.09,
+     5.0,  5.0, 0.0, 0.55, 0.24, 0.09,
+     5.0, -5.0, 0.0, 0.55, 0.24, 0.09,
+
+    -4.0, -4.0, 0.5, 0.803, 0.522, 0.247,
+    -4.5, -4.5, 0.5, 0.545, 0.271, 0.075,
+    -4.5,  4.5, 0.5, 0.545, 0.271, 0.075,
+    -4.0,  4.0, 0.5, 0.803, 0.522, 0.247,
 };
 
-unsigned int indices[12] = {
+unsigned int indices[16] = {
     0, 1, 2, 3,
     4, 5, 6, 7,
-    8, 9, 10, 11
+    8, 9, 10, 11,
+    12, 13, 14, 15,
 };
 
-glm::vec3 scale_vector = {0.1f, 0.1f, 0.1f};
-glm::vec3 rotation_vector = {0.2f, 0.3f, 0.7f};
+glm::vec3 translation_vector = {0.0f, 0.0f, 5.0f};
+glm::vec3 scale_vector = {0.2f, 0.2f, 0.2f};
+glm::vec3 rotation_vector = {0.0f, 0.8f, 1.6f};
+
+float zoomOut = 1.0f;
+float aspect_ratio;
+
+class Camera
+{
+public:
+    glm::vec3 position;
+    glm::vec3 target;
+    glm::vec3 up;
+
+    float fov;
+    float nearClip, farClip;
+    float aspectRatio;
+
+    Camera(glm::vec3 pos, glm::vec3 tgt, glm::vec3 upDir, float fovDeg, float aspect, float near, float far)
+        : position(pos), target(tgt), up(upDir), fov(fovDeg), nearClip(near), farClip(far), aspectRatio(aspect) {}
+
+    glm::mat4 getViewMatrix() {
+        return glm::lookAt(position, target, up);
+    }
+
+    glm::mat4 getProjectionMatrix() {
+        return glm::perspective(glm::radians(fov), aspect_ratio, nearClip, farClip);
+    }
+};
+
+Camera camera(
+    glm::vec3(0.0f, 0.0f, 10.0f),  // Position
+    glm::vec3(0.0f, 0.0f, 0.0f),   // Target
+    glm::vec3(0.0f, 1.0f, 0.0f),   // Up direction
+    45.0f,                         // FOV
+    aspect_ratio,                  // Aspect ratio
+    1.0f,                          // Near clip
+    20.0f                          // Far clip
+);
+
+void print_matrix(glm::mat4 mat)
+{
+    for (int i=0; i<4; i++)
+    {
+        for (int j=0; j<4; j++)
+        {
+            std::cout << mat[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
 
 void initialization()
 {
     glEnable(GL_BLEND);
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CW);
+    glCullFace(GL_BACK);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_DEPTH_TEST);
 }
@@ -58,11 +118,13 @@ void renderChessBoard()
 
         out vec3 fragColor;
 
-        uniform mat4 transform;
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 projection;
 
         void main()
         {
-            gl_Position = transform * vec4(position, 1.0);
+            gl_Position = projection * view * model * vec4(position, 1.0);
             fragColor = color;
         }
     )";
@@ -73,7 +135,7 @@ void renderChessBoard()
 
         void main()
         {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            color = vec4(fragColor, 1.0);
         }
     )";
 
@@ -149,18 +211,26 @@ void renderChessBoard()
 
     // Position attribute (location 0)
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 
     glGenBuffers(1, &IBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glm::mat4 transform = glm::toMat4(glm::quat(rotation_vector)) * glm::scale(glm::mat4(1.0f), scale_vector);
-    GLint location = glGetUniformLocation(program, "transform");
-    glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(transform));
+    glm::mat4 model = glm::toMat4(glm::quat(rotation_vector)) * glm::scale(glm::mat4(1.0f), scale_vector);
+    glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+    glm::mat4 projection = camera.getProjectionMatrix();
+    glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+    glm::mat4 view = camera.getViewMatrix();
+    glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_FALSE, glm::value_ptr(view));
     
     glBindVertexArray(VAO);
-    glDrawElements(GL_QUADS, 12, GL_UNSIGNED_INT, nullptr);
+    glDrawElements(GL_QUADS, 16, GL_UNSIGNED_INT, nullptr);
     glUseProgram(0);
 }
 
@@ -182,7 +252,6 @@ void initImGui(GLFWwindow *window)
     }
 
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable; 
 
@@ -196,7 +265,8 @@ void renderImGui()
     ImGui::NewFrame();
 
     ImGui::Begin("Debug Console");
-    ImGui::DragFloat3("Object Rotation", glm::value_ptr(rotation_vector), 0.1, 0, 1);
+    ImGui::DragFloat3("Object Rotation", glm::value_ptr(rotation_vector), 0.1f, 0.0f, 5.0f);
+    ImGui::DragFloat("Zoom out", &zoomOut, 0.1f, 1.0f, 10.0f);
     ImGui::End();
 
     ImGui::Render();
@@ -228,11 +298,14 @@ int main()
 
     initImGui(window);
 
+    
+
     while (!glfwWindowShouldClose(window))
     {        
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
+        aspect_ratio = (float)display_w / (float)display_h;
 
         displayFunction();
         renderImGui();
